e259efebb042f97eca5c83cebf856d45311697b4
ErShani <er.divyesh.shani@gmail.com>
2018-07-09 19:15:18 +0530

    Message form

--- public/js/index.js
+++ public/js/index.js
@@ -7,7 +7,19 @@ socket.on('disconnect', function () {
     console.log('Disconnected from server')
 })
 
-socket.on('newMessage', function(data){
-    console.log('New Message',data)
+socket.on('newMessage', function (data) {
+    console.log('New Message', data)
+    var li = $('<li></li>')
+    li.text(`${data.from}:${data.text}`)
+    $('#msgs').append(li)
 })
- 
+
+jQuery('#message-form').on('submit', function (e) {
+    e.preventDefault()
+    socket.emit('createMessage', {
+        from: 'Divyesh',
+        text: $('[name=message]').val()
+    }, function (data) {
+        console.log('Got it..!!!', data)
+    })
+})
--- server/server.js
+++ server/server.js
@@ -20,10 +20,11 @@ io.on('connection', (socket) => {
 
     socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'))
 
-    socket.on('createMessage', (message) => {
+    socket.on('createMessage', (message, callback) => {
         console.log('Created Msg', message)
 
-        socket.broadcast.emit('newMessage', generateMessage(message.from, message.text))
+        io.emit('newMessage', generateMessage(message.from, message.text))
+        callback('from the server');
     })
 
     socket.on('disconnect', () => {
